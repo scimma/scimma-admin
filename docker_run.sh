d0a780b389529932b927d94b083104077eca98c7
Spencer Nelson <swnelson@uw.edu>
2020-06-12 20:23:00 -0700

    Remove obsolete docker run scripts

--- docker_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run \
-       --rm \
-       --interactive \
-       --tty \
-       585193511743.dkr.ecr.us-west-2.amazonaws.com/scimma-admin-web
--- docker_run_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run \
-       --rm \
-       --volume $PWD:/app \
-       -p 8000:8000 \
-       -it \
-       585193511743.dkr.ecr.us-west-2.amazonaws.com/scimma-admin \
-       /usr/local/bin/python /app/scimma_admin/manage.py runserver
