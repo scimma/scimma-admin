ec576e0b8d9bc7c2c91774214b5b23b55df30957
Spencer Nelson <swnelson@uw.edu>
2020-06-12 20:23:15 -0700

    Detach from container when running it in a deploy

--- do_deploy.sh
+++ do_deploy.sh
@@ -5,6 +5,7 @@ $(aws ecr get-login --no-include-email --region us-west-2)
 docker pull 585193511743.dkr.ecr.us-west-2.amazonaws.com/scimma-admin-web:latest
 docker stop scimma-admin-web
 docker run --rm \
+           --detach \
            --env SCIMMA_ADMIN_PROD=yes \
            --publish 80:8000 \
            --name scimma-admin-web \
