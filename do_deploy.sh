14017f88305aca47af3133cfa65d2c91c2543ae9
Spencer Nelson <swnelson@uw.edu>
2020-06-22 13:00:33 -0700

    Fix deploy scripts

--- do_deploy.sh
+++ do_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i scimma-admin-web-deploy.pem ec2-user@admin.dev.hop.scimma.org '/bin/bash -c "
+ssh -i scimma-admin-web-deploy.pem ec2-user@admin-instance.dev.hop.scimma.org '/bin/bash -c "
 $(aws ecr get-login --no-include-email --region us-west-2)
 docker pull 585193511743.dkr.ecr.us-west-2.amazonaws.com/scimma-admin-web:latest
 docker stop scimma-admin-web
@@ -10,4 +10,5 @@ docker run --rm \
            --publish 80:8000 \
            --name scimma-admin-web \
            585193511743.dkr.ecr.us-west-2.amazonaws.com/scimma-admin-web:latest
+docker exec scimma-admin-web python manage.py migrate
 "'
