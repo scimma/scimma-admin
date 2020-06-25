31dc0d36cdd8ee622786bfade98299fe561841e5
Spencer Nelson <swnelson@uw.edu>
2020-06-25 16:22:46 -0700

    Add barebones UI for looking at creds

--- scimma_admin/hopskotch_auth/urls.py
+++ scimma_admin/hopskotch_auth/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
+    path("delete", views.delete, name="delete"),
+    path("create", views.create, name="create"),
 ]
--- scimma_admin/hopskotch_auth/views.py
+++ scimma_admin/hopskotch_auth/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 
+@login_required
 def index(request):
-    return HttpResponse("Hello, world.")
-# Create your views here.
+    creds = []
+    return render(
+        request, 'hopskotch_auth/index.html',
+        dict(creds=creds),
+    )
 
 
 def login(request):
-    return render(request, 'hopskotch_auth/login.html')
+    if request.user.is_authenticated:
+        return redirect(settings.LOGIN_REDIRECT_URL)
+    creds = []
+    return render(
+        request, 'hopskotch_auth/login.html',
+        dict(creds=creds),
+    )
 
 
 def logout(request):
     return HttpResponse("you're logged out!")
+
+
+@login_required
+def create(request):
+    return HttpResponse("not implemented")
+
+
+@login_required
+def delete(request):
+    return HttpResponse("not implemented")
