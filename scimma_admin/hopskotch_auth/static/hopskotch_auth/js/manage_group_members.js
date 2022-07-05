3c1acc2ee0543a3a7cfe3862489e8827cff5ccb1
Newsted, Jacob <newsted1@msu.edu>
2022-07-05 08:59:30 -0400

    Adding and removing members from groups done

--- scimma_admin/hopskotch_auth/static/hopskotch_auth/js/manage_group_members.js
+++ scimma_admin/hopskotch_auth/static/hopskotch_auth/js/manage_group_members.js
@@ -22,6 +22,41 @@ String.prototype.format = function () {
   }
   
   $(document).ready(function() {
+
+    added_template = '<tr scope="row">\
+                      <td class="mem_id">{}</td>\
+                      <td class="mem_name">{}</td>\
+                      <td class="mem_email">{}</td>\
+                      <td><select class="mem_perm"><option selected>Member</option><option>Owner</option></select></td>\
+                      <td class="remove_button"><button type="submit" class="btn btn-danger removeFrom">Remove</button></td>\
+                      </tr>'
+    avail_template = '<tr>\
+                      <td class="add_id">{}</td>\
+                      <td class="add_name">{}</td>\
+                      <td class="add_email">{}</td>\
+                      <td><button type="button" class="btn btn-primary addToCur">Add</button></td>\
+                      </tr>'
+    
+    avail_table = $('#avail_members').DataTable({
+        'info': false,
+        'columns': [
+            null,
+            null,
+            null,
+            {'searchable': false, 'orderable': false}
+        ]
+    });
+
+    added_table = $('#added_members').DataTable({
+        'info': false,
+        'columns': [
+            null,
+            null,
+            null,
+            {'searchable': false},
+            {'searchable': false, 'orderable': false}
+        ]
+    });
     function onAddMemberCallback() {
         var trElem = $(this).closest('tr');
         var username = trElem.find('td.add_id').text();
@@ -37,6 +72,9 @@ String.prototype.format = function () {
     }
 
     function addMember(groupname, username, trElem) {
+        var mem_id = $(trElem).find('td.add_id').text();
+        var mem_name = $(trElem).find('td.add_name').text();
+        var mem_email = $(trElem).find('td.add_email').text();
         gam_link = $('#gam_url').data().link;
         $.ajax({
             url: gam_link,
@@ -50,7 +88,13 @@ String.prototype.format = function () {
                 username: username
             },
             success: function (data, textStatus, jqXHR){
-                trElem.remove();
+                var row = avail_table.row(trElem);
+                row.remove().draw();
+                $('#added_members > tbody:last-child').append(added_template.format(mem_id, mem_name, mem_email));
+                var empty_table = $('#added_members').find('.dataTables_empty');
+                if(empty_table.length > 0) {
+                    empty_table.remove();
+                }
             },
             error: function(jqXHR, textStatus, errorThrown){
                 console.log('Error: ' + errorThrown);
@@ -62,6 +106,9 @@ String.prototype.format = function () {
 
     function removeMember(groupname, username, trElem) {
         grm_link = $('#grm_url').data().link;
+        var mem_id = $(trElem).find('td.mem_id').text();
+        var mem_name = $(trElem).find('td.mem_name').text();
+        var mem_email = $(trElem).find('td.mem_email').text();
         $.ajax({
             url: grm_link,
             method: "POST",
@@ -74,7 +121,13 @@ String.prototype.format = function () {
                 username: username
             },
             success: function (data, textStatus, jqXHR){
-                trElem.remove();
+                var row = added_table.row(trElem);
+                row.remove().draw();
+                $('#avail_members > tbody:last-child').append(avail_template.format(mem_id, mem_name, mem_email));
+                var empty_table = $('#avail_members').find('.dataTables_empty');
+                if(empty_table.length > 0) {
+                    empty_table.remove();
+                }
             },
             error: function(jqXHR, textStatus, errorThrown){
                 console.log('Error: ' + errorThrown);
