46bb75dcfba6e225c6308034fc7fc1a850954e19
cnweaver <cnweaver@gmail.com>
2024-08-20 17:04:12 -0400

    Rework topic permissions UI to match the updated credential permissions UI

--- scimma_admin/hopskotch_auth/static/hopskotch_auth/js/manage_topic.js
+++ scimma_admin/hopskotch_auth/static/hopskotch_auth/js/manage_topic.js
@@ -42,13 +42,6 @@ table_2_format = '<tr scope="row">\
 
 $(document).ready(function() {
     all_perms = ['Read', 'Write', 'Create', 'Delete', 'Alter', 'Describe', 'ClusterAction', 'DescribeConfigs', 'AlterConfigs', 'IdempotentWrite'];
-    edit_modal = null;
-    function initializeModal() {
-        modalElem = $('#topicPermEditModal');
-        edit_modal = new bootstrap.Modal($('#topicPermEditModal'), {
-          keyboard: false
-        })
-      }
 
     $('#id-selectOwnerForm #submit-id-cancel').click( function(event) {
         event.preventDefault();
@@ -68,7 +61,7 @@ $(document).ready(function() {
         'info': false,
         'columns': [
             {'className': 'group_name'},
-            {'className': 'remove_button', 'searchable': false, 'orderable': false}
+            {'className': 'permissions', 'searchable': false, 'orderable': false},
         ]
     });
 
@@ -76,10 +69,11 @@ $(document).ready(function() {
         'info': false,
         'columns': [
             {'className': 'group_name'},
-            {'className': 'perm_fields', 'searchable': false, 'orderable': false},
-            {'className': 'remove_button', 'searchable': false, 'orderable': false},
+            {'className': 'permissions', 'searchable': false, 'orderable': false},
         ]
     })
+    
+    progress_spinner = "<span class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span>";
 
     function addedGroupCallback() {
         var trElem = $(this).closest('tr');
@@ -236,14 +230,103 @@ $(document).ready(function() {
             traditional: true,
         });
     }
+    
+    function addPermCallback(){
+        var spanElem = $(this).closest('span');
+        var trElem = $(this).closest('tr');
+        var groupname = $(trElem).find('td.group_name').text();
+        var topicname = $('#id_owning_group_field').val() + '.' + $('#id_name_field').val();
+        var op_name = $(spanElem).contents().filter(function(){return this.nodeType == Node.TEXT_NODE; }).text().trim();
+        var readLink = $('#agp_url').data().link;
+        
+        $(this).addClass("disabled");
+        $(this).text("");
+        $(this).prepend(progress_spinner);
+        
+        $.ajax({
+            url: readLink,
+            method: "POST",
+            dataType: "json",
+            headers: {
+                "X-CSRFToken": getCookie('csrftoken')
+            },
+            data: {
+                topicname: topicname,
+                groupname: groupname,
+                permissions: op_name,
+            },
+            success: function (data, textStatus, jqXHR){
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                $(this).removeClass("disabled");
+                $(this).text("Add");
+                //TODO: show error to user
+                console.log('Error: ' + errorThrown);
+            },
+            complete: function(jqXHR, textStatus) {
+                //add to other table, creating row if necessary
+                let cur_row=added_table.row((idx, data) => data[0] == groupname)
+                if(cur_row.length==0)
+                    cur_row=added_table.row.add([groupname, ""]).draw(false);
+                var otherTr=cur_row.node();
+                $(otherTr).find('td.permissions').append("<span display=\"inline-block\">"+op_name+"&nbsp;<button role=\"button\" style=\"padding-top: 0; padding-bottom:0;\" class=\"btn btn-sm btn-danger remPerm objectModifier\">Remove</button></span> <br>");
+                //remove from this table, removing the whole row if empty
+                spanElem.remove();
+                var items = $(trElem).find('td.permissions span').length;
+                if(items==0)
+                    avail_table.row(trElem).remove().draw(false);
+            }
+        });
+    }
+    
+    function remPermCallback(){
+        var spanElem = $(this).closest('span');
+        var trElem = $(this).closest('tr');
+        var groupname = $(trElem).find('td.group_name').text();
+        var topicname = $('#id_owning_group_field').val() + '.' + $('#id_name_field').val();
+        var op_name = $(spanElem).contents().filter(function(){return this.nodeType == Node.TEXT_NODE; }).text().trim();
+        var readLink = $('#rgp_url').data().link;
+        var items = $(trElem).find('td.permissions span').length; //should be computed only when ready to move elements
+        $(this).addClass("disabled");
+        $(this).text("");
+        $(this).prepend(progress_spinner);
+        
+        $.ajax({
+            url: readLink,
+            method: "POST",
+            dataType: "json",
+            headers: {
+                "X-CSRFToken": getCookie('csrftoken')
+            },
+            data: {
+                topicname: topicname,
+                groupname: groupname,
+                permissions: op_name,
+            },
+            success: function (data, textStatus, jqXHR){
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                $(this).removeClass("disabled");
+                $(this).text("Remove");
+                //TODO: show error to user
+                console.log('Error: ' + errorThrown);
+            },
+            complete: function(jqXHR, textStatus) {
+                //add to other table, creating row if necessary
+                let cur_row=avail_table.row((idx, data) => data[0] == groupname)
+                if(cur_row.length==0)
+                    cur_row=avail_table.row.add([groupname, ""]).draw(false);
+                var otherTr=cur_row.node();
+                $(otherTr).find('td.permissions').append("<span display=\"inline-block\">"+op_name+"&nbsp;<button role=\"button\" style=\"padding-top: 0; padding-bottom:0;\" class=\"btn btn-sm btn-primary addPerm objectModifier\">Add</button></span> <br>");
+                //remove from this table, removing the whole row if empty
+                spanElem.remove();
+                var items = $(trElem).find('td.permissions span').length;
+                if(items==0)
+                    added_table.row(trElem).remove().draw(false);
+            }
+        });
+    }
 
-    initializeModal();
-
-    $('body').on('click', '.addToCur', addedGroupCallback);
-
-    $('body').on('click', '.removeFrom', removedGroupCallback);
-
-    $('body').on('click', '.readCheck', onReadCallback);
-
-    $('body').on('click', '.writeCheck', onWriteCallback);
+    $('body').on('click', '.addPerm', addPermCallback);
+    $('body').on('click', '.remPerm', remPermCallback);
 });
