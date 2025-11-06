f33e92fef2a11ed1eaf94361a964822bdcbdcc2c
dpetravick <dpetravick@yahoo.com>
2025-11-05 22:22:37 -0600

    config used to setup prod, dev tunnels

--- config.sh
+++ config.sh
@@ -1,20 +1,20 @@
-
+#!/bin/bash
 ##
-## This file is an overall configuration file for scimmma_adim
-## it is meant to be sourced, and supoport shell scripts
-## drive lall  server development environments. 
+## This file is an overall configuration file for scimmma_admin development
+## It's meant to be evaled in a overall driver shell script, e.g. ...
+## eval "$(./config.sh local)"
 ##
 ## Suporting postigras datasnes (archivne and scimma daming)
 ## may be local or accessed via a tunnel. 
 ##
-## CI (COnfuguratin itme) names engin wih r SECTERT_NAME or DB_NAME 
-## -l  Configure for local scimma_admin database, no archive databse
-## -d  Configure for access to AWS-resident development archive and admin databases. 
-## -p  Configure for access to AWS-resident producti0on archive and admin databases. 
+## CI (COnfuguration item 
+## local  Configure for local scimma_admin databases.
+## dev    Configure for tunnel access to AWS-resident development archive and admin databases. 
+## prod   Configure for tunnel access to AWS-resident producti0on archive and admin databases. 
 
 Help() {
-    echo "-prod or dev or local"  >&2
-    return 1
+    echo "config.sh <prod|dev|local>"  >&2
+    exit 1
 }
 
 get_tunnel_info() {
@@ -24,11 +24,12 @@ get_tunnel_info() {
 	       --output json
     }
 	  
-# purge local variables.
+# purge  variables and functions on exit
 cleanup () {
     unset system
     unset tunnel_info
-    unset get_tunnel_into
+    unset -f get_tunnel_info
+    unset -f Help
 }
 trap cleanup  EXIT INT TERM ERR
 
@@ -36,27 +37,24 @@ trap cleanup  EXIT INT TERM ERR
 if [ $# -eq 0 ]; then
     echo Arguements required >&2
     Help
-    return 1
 fi
 
 # Parse options
 system=$1
 if [[ "$system" == "prod" || "$system" == "dev" || "$system" == "local" ]]; then
-    echo configuring for system $system
+    echo configuring for system $system   >&2
 else
     Help
-    return 1
 fi
 # ensure all options are processed
 shift
 if [ $# -gt 0 ]; then
     echo "Error: Unrecognized arguments: $@" >&2
     Help
-    return 1
 fi
 
 # thsi might be a shim for integration.
-export SCIMMA_ENVIRONMENT=system
+echo export SCIMMA_ENVIRONMENT=system  >&2
 
 ##
 ## Core DJANGO
@@ -67,14 +65,14 @@ export SCIMMA_ENVIRONMENT=system
 #What is it       : the overall python logging level for DJOAN
 #Why Config       : debug can be useful in development, DEBUG must not be used in prod.
 #normal Default   : INFO
-export DJANGO_DEBUG=INFO
+echo export DJANGO_DEBUG=INFO
 
 #aettings.py name : SECRET_KEY
 #CI_Name          : SECRET_KEY_SECRET_NAME
 #What is it       : A secret used internally by django to encrypt things.
 #Why Config       : prod and dev deployment get the secret key  from AWS
 #Why Config       : LOCAL_DEV just makes one up ("zzzlocal") as its' transient.
-export SECRET_KEY_=zzzlocal
+echo export SECRET_KEY=zzzlocal
 
 
 ##
@@ -103,60 +101,60 @@ SYMPA_CREDS={}
 # Why Config      : Advancement of the scimma_admin schema is done on local postgress schema.
 # Default         : None Must be set via command line
 
-echo "$system"
+echo "$system"  >&2
 
 if [ "$system" = "prod" ]; then
     echo "environment configured for tunneling to prod databases"
     # settings.oy CIs (recall these are also terraform CIs)
-    export ARCHIVE_DB_INSTANCE_NAME=hopprod-archive-ingest-db
-    export ARCHIVE_DB_SECRET_NAME=hopProd-archive-ingest-db-password
-    export ADMIN_DB_INSTANCE_NAME=prod-scimma-admin-postgres
-    export ADMIN_DB_SECRET_NAME=prod-scimma-admin-db-password
+    echo export ARCHIVE_DB_INSTANCE_NAME=hopprod-archive-ingest-db
+    echo export ARCHIVE_DB_SECRET_NAME=hopProd-archive-ingest-db-password
+    echo export ADMIN_DB_INSTANCE_NAME=prod-scimma-admin-postgres
+    echo export ADMIN_DB_SECRET_NAME=prod-scimma-admin-db-password
 
     #tunnel support CIs (Nothing to do with terraform) 
     tunnel_info=$(get_tunnel_info $ARCHIVE_DB_INSTANCE_NAME)
-    export ARCHIVE_TUNNEL_BASTION="scotch.prod.hop.scimma.org"
-    export ARCHIVE_TUNNEL_REMOTE_HOST=$(echo $tunnel_info | jq -r '.Endpoint.Address')
-    export ARCHIVE_TUNNEL_REMOTE_PORT=$(echo $tunnel_info | jq -r '.Endpoint.Port')
-    export ARCHIVE_TUNNEL_LOCAL_PORT=54361
+    echo export ARCHIVE_TUNNEL_BASTION="scotch.prod.hop.scimma.org"
+    echo export ARCHIVE_TUNNEL_REMOTE_HOST=$(echo $tunnel_info | jq -r '.Endpoint.Address')
+    echo export ARCHIVE_TUNNEL_REMOTE_PORT=$(echo $tunnel_info | jq -r '.Endpoint.Port')
+    echo export ARCHIVE_TUNNEL_LOCAL_PORT=54361
     tunnel_info=$(get_tunnel_info $ADMIN_DB_INSTANCE_NAME)
-    export ADMIN_TUNNEL_BASTION="scotch.prod.hop.scimma.org"
-    export ADMIN_TUNNEL_REMOTE_HOST=$(echo $tunnel_info | jq -r '.Endpoint.Address')
-    export ADMIN_TUNNEL_REMOTE_PORT=$(echo $tunnel_info | jq -r '.Endpoint.Port')
-    export ADMIN_TUNNEL_LOCAL_PORT=54361
-    export ADMIN_DB_LOCAL_PORT=54360
+    echo export ADMIN_TUNNEL_BASTION="scotch.prod.hop.scimma.org"
+    echo export ADMIN_TUNNEL_REMOTE_HOST=$(echo $tunnel_info | jq -r '.Endpoint.Address')
+    echo export ADMIN_TUNNEL_REMOTE_PORT=$(echo $tunnel_info | jq -r '.Endpoint.Port')
+    echo export ADMIN_TUNNEL_LOCAL_PORT=54360
 
 elif [ "$system" = "dev" ]; then
-    echo "environment configured for tunneling to dev databases"
+    echo "environment configured for tunneling to dev databases"  >&2
     # settings.oy CIs (recall these are also terraform CIs)
-    export ARCHIVE_DB_INSTANCE_NAME=hopdevel-archive-ingest-db
-    export ARCHIVE_DB_SECRET_NAME=hopDevel-archive-ingest-db-password
-    export ADMIN_DB_INSTANCE_NAME=scimma-admin-postgres
-    export ADMIN_DB_SECRET_NAME=scimma-admin-db-password
+    echo export ARCHIVE_DB_INSTANCE_NAME=hopdevel-archive-ingest-db
+    echo export ARCHIVE_DB_SECRET_NAME=hopDevel-archive-ingest-db-password
+    echo export ADMIN_DB_INSTANCE_NAME=scimma-admin-postgres
+    echo export ADMIN_DB_SECRET_NAME=scimma-admin-db-password
 
     #tunnel support CIs (Nothing to do with terraform) 
     tunnel_info=$(get_tunnel_info $ARCHIVE_DB_INSTANCE_NAME)
-    export ARCHIVE_TUNNEL_BASTION="scotch.dev.hop.scimma.org"
-    export ARCHIVE_TUNNEL_REMOTE_HOST=$(echo $tunnel_info | jq -r '.Endpoint.Address')
-    export ARCHIVE_TUNNEL_REMOTE_PORT=$(echo $tunnel_info | jq -r '.Endpoint.Port')
-    export ARCHIVE_TUNNEL_LOCAL_PORT=54361
+    echo export ARCHIVE_TUNNEL_BASTION="scotch.dev.hop.scimma.org"
+    echo export ARCHIVE_TUNNEL_REMOTE_HOST=$(echo $tunnel_info | jq -r '.Endpoint.Address')
+    echo export ARCHIVE_TUNNEL_REMOTE_PORT=$(echo $tunnel_info | jq -r '.Endpoint.Port')
+    echo export ARCHIVE_TUNNEL_LOCAL_PORT=54361
     tunnel_info=$(get_tunnel_info $ADMIN_DB_INSTANCE_NAME)
-    export ADMIN_TUNNEL_BASTION="scotch.dev.hop.scimma.org"
-    export ADMIN_TUNNEL_REMOTE_HOST=$(echo $tunnel_info | jq -r '.Endpoint.Address')
-    export ADMIN_TUNNEL_REMOTE_PORT=$(echo $tunnel_info | jq -r '.Endpoint.Port')
-    export ADMIN_TUNNEL_LOCAL_PORT=54361
+    echo export ADMIN_TUNNEL_BASTION="scotch.dev.hop.scimma.org"
+    echo export ADMIN_TUNNEL_REMOTE_HOST=$(echo $tunnel_info | jq -r '.Endpoint.Address')
+    echo export ADMIN_TUNNEL_REMOTE_PORT=$(echo $tunnel_info | jq -r '.Endpoint.Port')
+    echo export ADMIN_TUNNEL_LOCAL_PORT=54360
 else
-    export ARCHIVE_DB__NAME=postgres
-    export ARCHIVE_DB__USER=postgres
-    export ARCHIVE_DB_PASSWORD=postgres
-    export ARCHIVE_HOST="127.0.0.1"
-    export ARCHIVE_DB_PORT=5436
+    echo "environment configured local pastgres databases"  >&2
+    echo export ARCHIVE_DB__NAME=postgres
+    echo export ARCHIVE_DB__USER=postgres
+    echo export ARCHIVE_DB_PASSWORD=postgres
+    echo export ARCHIVE_HOST="127.0.0.1"
+    echo export ARCHIVE_DB_PORT=5433
     
-    export ADMIN_DB__NAME=postgres
-    export ADMIN_DB__USER=postgres
-    export ADMIN_DB_PASSWORD=postgres
-    export ADMIN_HOST="127.0.0.1"
-    export ADMIN_DB_PORT=5435
+    echo export ADMIN_DB__NAME=postgres
+    echo export ADMIN_DB__USER=postgres
+    echo export ADMIN_DB_PASSWORD=postgres
+    echo export ADMIN_HOST="127.0.0.1"
+    echo export ADMIN_DB_PORT=5432
 fi
 
 
@@ -168,7 +166,7 @@ fi
 #CI_Name          : SECURE_SSL_REDIRECT 
 #What is it : causes django re redirecet http to https automaticalls. 
 #Why Config : Some development is simpler of done on http, to avoind the work of setting u  https stuff.
-export SECURE_SSL_REDIRECT=false
+echo export SECURE_SSL_REDIRECT=false
 
 # Settings.py name : :KAFKA_USER_AUTH_GROUP
 # CI_Name          : KAFKA_USER_AUTH_GROUP
@@ -176,7 +174,7 @@ export SECURE_SSL_REDIRECT=false
 # Why Config       : dunno we have one keycloak and apparently one group shared between dev and prod
 # Override         : Value of env variable KAFKA_USER_AUTH_GROUP 
 #Assert            : None
-export KAFKA_USER_AUTH_GROUP="/Hopskotch Users"
+echo export KAFKA_USER_AUTH_GROUP="/Hopskotch Users"
 
 # Settings.py name : KAFKA_BROKER_URL
 # CI_Name          : KAFKA_BROKER_URL
@@ -184,13 +182,13 @@ export KAFKA_USER_AUTH_GROUP="/Hopskotch Users"
 # Why Config : dev and prod use different instances (what about Localdev??)
 # Override   : Value of env variable KAFKA_BROKER_URL 
 # Assert     : is not None in Prod, dev.
-export KAFKA_BROKER_URL=dog
+echo export KAFKA_BROKER_URL=dog
 
 # Settings.py name : OIDC_RP_CLIENT_ID
 # CI_Name          : OIDC_RP_CLIENT_ID
 # What is it : A client provided by the OIDC Provider
 #terraform? : scimma_admin.tf: "scimma_admin_keycloak_client_id"
-export OIDC_RP_CLIENT_ID=dog
+echo export OIDC_RP_CLIENT_ID=dog
 
 # Settings.py name : OIDC_RP_CLIENT_SECRET
 # CI_Name          : OIDC_RP_CLIENT_SECRET_NAME
@@ -198,7 +196,7 @@ export OIDC_RP_CLIENT_ID=dog
 # Why Config : TBD
 # terraform? : scimma_admin.tf "scimma_admin_keycloak_client_secret"
 # terraform? : scimma_admin.tf "cilogon_localdev_client_secret"
-export OIDC_RP_CLIENT_SECRET_NAME=dog
+echo export OIDC_RP_CLIENT_SECRET_NAME=dog
 
 
 
--- public_pages_setup.sh
+++ public_pages_setup.sh
@@ -1,123 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
-system=devel
+Help() {
+    echo "-p production, -d development -x  vebose -M don't migrate"  >&2
+    exit 1
+}
+system=dev
+migrate=true
 
 # Parse options
-while getopts ":xvdpj" opt; do
-  case $opt in 
-    v)
-      set -x
-      ;;
-    d)
-	system=devel
-	#export SCIMMA_ENVIRONMENT=devel
-      ;;
-    p)
-	system=prod
-	#export SCIMMA_ENVIRONMENT=prod
-      ;;	
-    h)
-	-v verbose -p use prod databses -d use devel databases -j just run locally 
-	;;
-    x)
-	set -x
-	;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
+while getopts "dpxh" opt; do
+  case $opt in
+    p) syatem=prod ;;
+    d) system=dev ;;
+    d) migrate=false ;;
+    h) Help ;;
+    x) set -x ;;
+    *) Help ;;
   esac
 done
+shift $((OPTIND -1))
+if [ $# -gt 0 ]; then
+    echo "Error: Unrecognized arguments: $@" >&2
+    Help
+fi
 
+eval $(./config.sh dev)
 
 #
 #  Assume these have been done once
 #  make localdev.conf
 #  cp scimma_admin/sample_local_settings.py scimma_admin/local_settings.py
-
+#  valid AWS credentials
 
 # Configuration
-# export SSL_CERT_FILE=/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/certifi/cacert.pem
-export SSL_CERT_FILE=/usr/local/etc/openssl/cert.pem
-
-
-
-export REMOTE_TUNNEL="True"  #signal laptop development
-export REMOTE_USER="don.petravick"
-export SSH_KEY="~/.ssh/id_rsa"  # Path to your SSH key
-
-export ARCHIVE_LOCAL_PORT=54320
-export ARCHIVE_REMOTE_PORT=5432
-
-export ADMIN_LOCAL_PORT=54321
-export ADMIN_REMOTE_PORT=5432
-
-export SCIMMA_ENVIRONMENT=dev
-
-echo "$system"
-
-get_secret () {
-    aws secretsmanager get-secret-value --secret-id $1  | jq -r .SecretString
-}
-
-
-
-if [ "$system" = "prod" ]; then
-    echo "System is production"
-    
-    export ARCHIVE_HOST="scotch.prod.hop.scimma.org"
-    export ARCHIVE_DNS=hopprod-archive-ingest-db.cgaf3c8se1sj.us-west-2.rds.amazonaws.com
-    export ARCHIVE_DB_INSTANCE_NAME=hopprod-archive-ingest-db
-    export ARCHIVE_DB_SECRET_NAME=hopProd-archive-ingest-db-password
-#    export ARCHIVE_DB_PASSWD=`get_secret hopProd-archive-ingest-db-password`
-#    export ARCHIVE_DB_USERNAME=archive_db
-#    export ARCHIVE_DB_DBNAME=archivedb
-
-    export ADMIN_HOST="scotch.prod.hop.scimma.org"
-    export ADMIN_DNS=prod-scimma-admin-postgres.cgaf3c8se1sj.us-west-2.rds.amazonaws.com
-    export ADMIN_DB_INSTANCE_NAME=prod-scimma-admin-postgres
-    export ADMIN_DB_SECRET_NAME=prod-scimma-admin-db-password
-#    export ADMIN_DB_PASSWD=`get_secret prod-scimma-admin-db-password`
-#    export ADMIN_DB_USERNAME=scimma_admin
-#    export ADMIN_DB_DBNAME=prod_scimma_admin_db
-    
-else
-    echo "System using devel system "
-    export ARCHIVE_HOST="scotch.dev.hop.scimma.org"
-    export ARCHIVE_DNS=hopdevel-archive-ingest-db.cgaf3c8se1sj.us-west-2.rds.amazonaws.com
-    export ARCHIVE_DB_INSTANCE_NAME=hopdevel-archive-ingest-db
-    export ARCHIVE_DB_SECRET_NAME=hopDevel-archive-ingest-db-password
-#    export ARCHIVE_DB_PASSWD=`get_secret hopDevel-archive-ingest-db-password`
-#    export ARCHIVE_DB_USERNAME=archive_db
-#    export ARCHIVE_DB_DBNAME=archivedb
-
-    export ADMIN_HOST="scotch.dev.hop.scimma.org"
-    export ADMIN_DNS=scimma-admin-postgres.cgaf3c8se1sj.us-west-2.rds.amazonaws.com
-    export ADMIN_DB_INSTANCE_NAME=scimma-admin-postgres
-    export ADMIN_DB_SECRET_NAME=scimma-admin-db-password
-#    export ADMIN_DB_PASSWD=`get_secret scimma-admin-db-password`
-#    export ADMIN_DB_USERNAME=scimma_admin
-#    export ADMIN_DB_DBNAME=scimma_admin_db
-
-fi
-
+export SSL_CERT_FILE=/usr/local/etc/openssl/cert.pem     # DLP's machine needs this
+export REMOTE_TUNNEL="True"                              # signal laptop development
+export REMOTE_USER="don.petravick"                       # user name bastion machine
+export SSH_KEY="~/.ssh/id_rsa"  # Path to your SSH key   # 
 
 rm -f nohup.out
 # Start Archive SSH tunnel in background
 #echo "Starting ARCHIVE SSH tunnel..."
-nohup ssh  -i $SSH_KEY -N -L $ARCHIVE_LOCAL_PORT:$ARCHIVE_DNS:5432 "$REMOTE_USER@$ARCHIVE_HOST" &  
+connect=$ARCHIVE_TUNNEL_LOCAL_PORT:$ARCHIVE_TUNNEL_REMOTE_HOST:$ARCHIVE_TUNNEL_REMOTE_PORT
+nohup ssh  -i $SSH_KEY -N -L $connect "$REMOTE_USER@$ARCHIVE_HOST" &  
 ARCHIVE_TUNNEL_PID=$!
-echo ARCHIVE_TUNNEL_PID
 
 # Start ADMIN SSH tunnel in background
 echo "Starting ADMIN SSH tunnel..."
-nohup ssh  -i $SSH_KEY -N -L $ADMIN_LOCAL_PORT:$ADMIN_DNS:5432 "$REMOTE_USER@$ADMIN_HOST" &  
+connect=$ADMIN_TUNNEL_LOCAL_PORT:$ADMIN_TUNNEL_REMOTE_HOST:$ADMIN_TUNNEL_REMOTE_PORT
+nohup ssh  -i $SSH_KEY -N -L $connect "$REMOTE_USER@$ADMIN_HOST" &  
 ADMIN_TUNNEL_PID=$!
-echo ADMIN_TUNNEL_PID
-
-sleep 2
-echo take a peek at nohup.out
-cat nohup.out  
-sleep 4
 
 # Cleanup function to kill the tunnel
 cleanup() {
@@ -135,17 +67,20 @@ cleanup() {
 # Trap signals and errors to ensure cleanup
 trap cleanup EXIT INT TERM ERR
 
-# Wait briefly to ensure tunnels is up
-sleep 2
+sleep 4
+echo cat-ing nohup.out -- it should be emptysleep 4
+cat nohup.out  
+Echo Ok moving on from loosing at nohup.com
 
-#admin
-#psql -h localhost -p 54321 -U scimma_admin -d scimma_admin_db -c '\dt' -e
-#psql -h localhost -p 54320 -U scimma_admin -d scimma_admin_db -c '\dt' -e
 
-# just do this so as to not think abou it.
-(cd scimma_admin ; python manage.py makemigrations)
-(cd scimma_admin ; python manage.py migrate)
 
+
+exit
+# just do this so as to not think about it.
+if [ $migrate = true  ] ; then  
+    (cd scimma_admin ; python manage.py makemigrations)
+    (cd scimma_admin ; python manage.py migrate)
+fi 
 #./scimma_admin/manage.py runserver === n.b. https not supported. 
 
 #uwsgi --chdir=scimma_admin --module=scimma_admin.wsgi:application \
